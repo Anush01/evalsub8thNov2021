7cabf0628ae6612d6746987cfb0a735d78a5acd7
ampmasai <02.anush@gmail.com>
2021-11-08 18:50:14 +0530

    made some ui changes

--- app/src/main/java/com/anushmp/randomusermvvm/views/RandomizeActivityView.kt
+++ app/src/main/java/com/anushmp/randomusermvvm/views/RandomizeActivityView.kt
@@ -47,6 +47,44 @@ class RandomizeActivityView : AppCompatActivity() {
 
         viewmodel.getrandomuser().observe(this,{
 
+            var out:String = it.Responsegender +
+                    "\n"+it.ResponseNametitle +
+                    "\n"+it.ResponseNamefirst+
+                    "\n"+it.ResponseNamelast+
+                    "\n"+it.ResponseLocationStreetNumber+
+                    "\n"+it.ResponseLocationStreetName+
+                    "\n"+it.ResponseLocationCity+
+                    "\n"+it.ResponseLocationState+
+                    "\n"+it.ResponseLocationCountry+
+                    "\n"+it.ResponseLocationpostcode+
+                    "\n"+it.ResponseLocationCoordinatesLatitude+
+                    "\n"+it.ResponseLocationCoordinatesLongitude+
+                    "\n"+it.ResponseLocationTimezoneOffset+
+                    "\n"+it.ResponseLocationTimezoneDescription+
+                    "\n"+it.Responseemail+
+                    "\n"+it.ResponseLoginuuid+
+                    "\n"+it.ResponseLoginusername+
+                    "\n"+it.ResponseLoginpassword+
+                    "\n"+it.ResponseLoginsalt+
+                    "\n"+it.ResponseLoginmd5+
+                    "\n"+it.ResponseLoginsha256+
+                    "\n"+it.ResponseDOBdate+
+                    "\n"+it.ResponseDOBage+
+                    "\n"+it.ResponseRegisteredDate+
+                    "\n"+it.ResponseRegisteredAge+
+                    "\n"+it.Responsephone+
+                    "\n"+it.Responsecell+
+                    "\n"+it.ResponseIDName+
+                    "\n"+it.ResponseIDValue+
+                    "\n"+it.ResponsePictureLarge+
+                    "\n"+it.ResponsePictureMedium+
+                    "\n"+it.ResponsePictureThumbnail+
+                    "\n"+it.Responsenat+
+                    "\n"+it.infoseed+
+                    "\n"+it.inforesults+
+                    "\n"+it.infopage+
+                    "\n"+it.infoversion+"\n"
+
             username.text = it.ResponseNamefirst
 
         })
