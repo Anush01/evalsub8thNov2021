cead4469715d1e3a8d6bd28b6a019b47c3d7aa4b
ampmasai <02.anush@gmail.com>
2021-11-08 14:36:31 +0530

    initial setup commit

--- app/src/main/java/com/anushmp/randomusermvvm/RandomUserView.kt
+++ app/src/main/java/com/anushmp/randomusermvvm/RandomUserView.kt
@@ -3,7 +3,7 @@ package com.anushmp.randomusermvvm
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class RandomUserView : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
