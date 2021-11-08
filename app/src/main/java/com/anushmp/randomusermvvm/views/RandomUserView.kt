6872ec57b2fbb61c3f6354fa61acd3d8cb2c2396
ampmasai <02.anush@gmail.com>
2021-11-08 18:03:15 +0530

    hooking up uis.

--- app/src/main/java/com/anushmp/randomusermvvm/views/RandomUserView.kt
+++ app/src/main/java/com/anushmp/randomusermvvm/views/RandomUserView.kt
@@ -1,8 +1,16 @@
 package com.anushmp.randomusermvvm.views
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
 import com.anushmp.randomusermvvm.R
+import com.anushmp.randomusermvvm.datamodels.local.RandomUserDao
+import com.anushmp.randomusermvvm.datamodels.local.RandomUserDataBase
+import com.anushmp.randomusermvvm.repositories.RandomUserRepository
+import com.anushmp.randomusermvvm.viewmodels.RandomUserViewModel
+import com.anushmp.randomusermvvm.viewmodels.RuViewModelFactory
 import kotlinx.android.synthetic.main.activity_main.*
 
 class RandomUserView : AppCompatActivity() {
@@ -16,13 +24,47 @@ class RandomUserView : AppCompatActivity() {
     //FirstTimeSetup
     //gotorandomuseractivity
 
+    lateinit var dao:RandomUserDao
+    lateinit var repo:RandomUserRepository
+    lateinit var viewModelFactory: ViewModelProvider.Factory
+    lateinit var viewmodel:RandomUserViewModel
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //data
+        dao = RandomUserDataBase.getDataBaseSingleton(this).getRandomUserdao()
+        repo = RandomUserRepository(dao)
 
 
+        //viewmodel
+        viewModelFactory = RuViewModelFactory(repo)
+        viewmodel = ViewModelProvider(this,viewModelFactory).get(RandomUserViewModel::class.java)
+
+
+
+
+
+
+        FirstTimeSetup.setOnClickListener {
+
+            viewmodel.FirstTimeApiCall()
+
+            Toast.makeText(this,"called the api for the first time",Toast.LENGTH_LONG).show()
+
+
+
+        }
+
+
+        gotoRandomUserActivity.setOnClickListener {
+
+            var intent = Intent(this,RandomizeActivityView::class.java)
+            startActivity(intent)
+
+        }
 
 
     }
--- app/src/main/java/com/anushmp/randomusermvvm/views/RandomizeActivityView.kt
+++ app/src/main/java/com/anushmp/randomusermvvm/views/RandomizeActivityView.kt
@@ -2,10 +2,18 @@ package com.anushmp.randomusermvvm.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import com.anushmp.randomusermvvm.R
+import com.anushmp.randomusermvvm.datamodels.local.RandomUserDao
+import com.anushmp.randomusermvvm.datamodels.local.RandomUserDataBase
+import com.anushmp.randomusermvvm.repositories.RandomUserRepository
+import com.anushmp.randomusermvvm.viewmodels.RandomUserViewModel
+import com.anushmp.randomusermvvm.viewmodels.RuViewModelFactory
 import kotlinx.android.synthetic.main.activity_randomize_view.*
 
 class RandomizeActivityView : AppCompatActivity() {
+
+
 
     //textviews
     //username
@@ -16,18 +24,42 @@ class RandomizeActivityView : AppCompatActivity() {
     //FirstTimeSetup
     //gotorandomuseractivity
 
+
+    lateinit var dao: RandomUserDao
+    lateinit var repo: RandomUserRepository
+    lateinit var viewModelFactory: ViewModelProvider.Factory
+    lateinit var viewmodel: RandomUserViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_randomize_view)
-    }
 
 
-    private fun setupviews() {
+        //data
+        dao = RandomUserDataBase.getDataBaseSingleton(this).getRandomUserdao()
+        repo = RandomUserRepository(dao)
+
+
+        //viewmodel
+        viewModelFactory = RuViewModelFactory(repo)
+        viewmodel = ViewModelProvider(this,viewModelFactory).get(RandomUserViewModel::class.java)
+
+
+        viewmodel.getrandomuser().observe(this,{
+
+            username.text = it.ResponseNamefirst
+
+        })
+
+
 
         getrandomuser.setOnClickListener {
 
+            viewmodel.updaterandomuser()
 
         }
 
+
     }
+
 }
